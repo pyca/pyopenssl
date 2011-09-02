5e99f440d1a8cd00f11ef569a98ba6a18a8ccd37
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2011-09-02 11:08:10 -0400

    Bump the version to the final release version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from distutils.errors import DistutilsFileError
 from distutils.command.build_ext import build_ext
 
 # XXX Deduplicate this
-__version__ = '0.13a1'
+__version__ = '0.13'
 
 crypto_src = ['OpenSSL/crypto/crypto.c', 'OpenSSL/crypto/x509.c',
               'OpenSSL/crypto/x509name.c', 'OpenSSL/crypto/pkey.c',
