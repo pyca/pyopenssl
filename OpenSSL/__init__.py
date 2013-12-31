4a2592337e82bcfc5d6c80e7b76a20a7913b1076
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2013-12-31 13:49:17 -0500

    just drop this for now

--- OpenSSL/__init__.py
+++ OpenSSL/__init__.py
@@ -6,7 +6,6 @@ pyOpenSSL - A simple wrapper around the OpenSSL library
 """
 
 from OpenSSL import rand, crypto, SSL
-from OpenSSL import tsafe
 from OpenSSL.version import __version__
 
 __all__ = [
