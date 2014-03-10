2cffdbc96f26052351d5ce7ec3e3ce738e7454ab
Fedor Brunner <fedor.brunner@azet.sk>
2014-03-10 10:35:23 +0100

    Test methods test_get_cipher_* have been split into two sets. assertTrue(isinstance was replaced with assertIsInstance. assertEqual for None was replaced with assertIs. Added entries to doc/api/ssl.rst.

--- OpenSSL/test/test_ssl.py
+++ OpenSSL/test/test_ssl.py
@@ -1932,60 +1932,72 @@ class ConnectionTests(TestCase, _LoopbackMixin):
 
     # XXX want_read
 
+    def test_get_cipher_name_before_connect(self):
+        """
+        :py:obj:`Connection.get_cipher_name` returns  :py:obj:`None`
+        if no connection has been established.
+        """
+        ctx = Context(TLSv1_METHOD)
+        conn = Connection(ctx, None)
+        self.assertIs(conn.get_cipher_name(), None)
+
     def test_get_cipher_name(self):
         """
         :py:obj:`Connection.get_cipher_name` returns the name of the currently
-                 used cipher or :py:obj:`None` if no connection has been established. 
+        used cipher.
         """
-        # if connection is not established Connection.cipher returns None.
-        ctx = Context(TLSv1_METHOD)
-        conn = Connection(ctx, None)
-        self.assertEqual(conn.get_cipher_name(), None)
-
         server, client = self._loopback()
         server_cipher_name, client_cipher_name = \
             server.get_cipher_name(), client.get_cipher_name()
 
-        self.assertTrue(isinstance(server_cipher_name, str))
-        self.assertTrue(isinstance(client_cipher_name, str))
+        self.assertIsInstance(server_cipher_name, str)
+        self.assertIsInstance(client_cipher_name, str)
 
         self.assertEqual(server_cipher_name, client_cipher_name)
+
+    def test_get_cipher_version_before_connect(self):
+        """
+        :py:obj:`Connection.get_cipher_version` returns :py:obj:`None`
+        if no connection has been established.
+        """
+        ctx = Context(TLSv1_METHOD)
+        conn = Connection(ctx, None)
+        self.assertIs(conn.get_cipher_version(), None)
 
     def test_get_cipher_version(self):
         """
         :py:obj:`Connection.get_cipher_version` returns the protocol name of the currently
-                 used cipher or :py:obj:`None` if no connection has been established. 
+        used cipher.
         """
-        # if connection is not established Connection.cipher returns None.
-        ctx = Context(TLSv1_METHOD)
-        conn = Connection(ctx, None)
-        self.assertEqual(conn.get_cipher_version(), None)
-
         server, client = self._loopback()
         server_cipher_version, client_cipher_version = \
             server.get_cipher_version(), client.get_cipher_version()
 
-        self.assertTrue(isinstance(server_cipher_version, str))
-        self.assertTrue(isinstance(client_cipher_version, str))
+        self.assertIsInstance(server_cipher_version, str)
+        self.assertIsInstance(client_cipher_version, str)
 
         self.assertEqual(server_cipher_version, client_cipher_version)
+
+    def test_get_cipher_bits_before_connect(self):
+        """
+        :py:obj:`Connection.get_cipher_bits` returns :py:obj:`None`
+        if no connection has been established.
+        """
+        ctx = Context(TLSv1_METHOD)
+        conn = Connection(ctx, None)
+        self.assertIs(conn.get_cipher_bits(), None)
 
     def test_get_cipher_bits(self):
         """
         :py:obj:`Connection.get_cipher_bits` returns the number of secret bits of the currently
-                 used cipher or :py:obj:`None` if no connection has been established. 
+        used cipher.
         """
-        # if connection is not established Connection.cipher returns None.
-        ctx = Context(TLSv1_METHOD)
-        conn = Connection(ctx, None)
-        self.assertEqual(conn.get_cipher_bits(), None)
-
         server, client = self._loopback()
         server_cipher_bits, client_cipher_bits = \
             server.get_cipher_bits(), client.get_cipher_bits()
 
-        self.assertTrue(isinstance(server_cipher_bits, int))
-        self.assertTrue(isinstance(client_cipher_bits, int))
+        self.assertIsInstance(server_cipher_bits, int)
+        self.assertIsInstance(client_cipher_bits, int)
 
         self.assertEqual(server_cipher_bits, client_cipher_bits)
 
