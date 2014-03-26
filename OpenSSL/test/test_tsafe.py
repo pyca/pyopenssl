5bfdea35063a84eee07f45fa00daacaa5639e6dd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-03-25 21:01:53 -0400

    Further simplifications.

--- OpenSSL/test/test_tsafe.py
+++ OpenSSL/test/test_tsafe.py
@@ -8,7 +8,6 @@ Unit tests for :py:obj:`OpenSSL.tsafe`.
 from OpenSSL.SSL import TLSv1_METHOD, Context
 from OpenSSL.tsafe import Connection
 from OpenSSL.test.util import TestCase
-from OpenSSL.test.test_ssl import _create_certificate_chain
 
 
 class ConnectionTest(TestCase):
@@ -19,15 +18,7 @@ class ConnectionTest(TestCase):
         """
         :py:obj:`OpenSSL.tsafe.Connection` can be instantiated.
         """
-        chain = _create_certificate_chain()
-        [(_, _), (ikey, icert), (skey, scert)] = chain
-
-        # Create the server context
-        ctx = Context(TLSv1_METHOD)
-        ctx.use_privatekey(skey)
-        ctx.use_certificate(scert)
-
         # The following line should not throw an error.  This isn't an ideal
         # test.  It would be great to refactor the other Connection tests so
         # they could automatically be applied to this class too.
-        Connection(ctx, None)
+        Connection(Context(TLSv1_METHOD), None)
