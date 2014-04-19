d5839e2a950d475b518023b23dee06862aa5ba77
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-04-19 09:26:44 -0400

    strings are hard, be explicit

--- OpenSSL/crypto.py
+++ OpenSSL/crypto.py
@@ -364,6 +364,10 @@ def get_elliptic_curve(name):
 
     See :py:func:`get_elliptic_curves` for information about curve objects.
 
+    :param name: The OpenSSL short name identifying the curve object to
+        retrieve.
+    :type name: :py:class:`unicode`
+
     If the named curve is not supported then :py:class:`ValueError` is raised.
     """
     for curve in get_elliptic_curves():
