bae2dc36f3a4ceccca6634f2989927d004b76d37
Alex Gaynor <alex.gaynor@gmail.com>
2014-08-14 13:44:54 -0700

    Fix for py3k

--- OpenSSL/test/test_util.py
+++ OpenSSL/test/test_util.py
@@ -5,6 +5,6 @@ from OpenSSL.test.util import TestCase
 
 class TestErrors(TestCase):
     def test_exception_from_error_queue_nonexistant_reason(self):
-        lib.ERR_put_error(lib.ERR_LIB_EVP, 0, 1112, "", 10)
+        lib.ERR_put_error(lib.ERR_LIB_EVP, 0, 1112, b"", 10)
         exc = self.assertRaises(ValueError, exception_from_error_queue, ValueError)
         self.assertEqual(exc.args[0][0][2], "")
