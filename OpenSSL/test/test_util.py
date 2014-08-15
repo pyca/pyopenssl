9823239397c17e6dda1e2813ecc8cc4b48f4cbf8
Alex Gaynor <alex.gaynor@gmail.com>
2014-08-15 11:23:00 -0700

    A docstring for the test

--- OpenSSL/test/test_util.py
+++ OpenSSL/test/test_util.py
@@ -5,6 +5,11 @@ from OpenSSL.test.util import TestCase
 
 class TestErrors(TestCase):
     def test_exception_from_error_queue_nonexistant_reason(self):
+        """
+        :py:func:`exception_from_error_queue` does not raise a ``TypeError``
+        when it encounters an OpenSSL error code which does not have a reason
+        string.
+        """
         lib.ERR_put_error(lib.ERR_LIB_EVP, 0, 1112, b"", 10)
         exc = self.assertRaises(ValueError, exception_from_error_queue, ValueError)
         self.assertEqual(exc.args[0][0][2], "")
