6b5d3813eb18fd77ec296930f0b6570914c837b9
Jim Shaver <dcypherd@gmail.com>
2015-04-25 17:45:53 -0400

    Removed unnecessary escape characters.  Thanks @alex

--- examples/mk_simple_certs.py
+++ examples/mk_simple_certs.py
@@ -7,15 +7,15 @@ from certgen import *   # yes yes, I know, I'm lazy
 cakey = createKeyPair(TYPE_RSA, 2048)
 careq = createCertRequest(cakey, CN='Certificate Authority')
 cacert = createCertificate(careq, (careq, cakey), 0, (0, 60*60*24*365*5)) # five years
-print('Creating Certificate Authority private key in \"simple/CA.pkey\"')
+print('Creating Certificate Authority private key in "simple/CA.pkey"')
 open('simple/CA.pkey', 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, cakey))
-print('Creating Certificate Authority certificate in \"simple/CA.cert\"')
+print('Creating Certificate Authority certificate in "simple/CA.cert"')
 open('simple/CA.cert', 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cacert))
 for (fname, cname) in [('client', 'Simple Client'), ('server', 'Simple Server')]:
     pkey = createKeyPair(TYPE_RSA, 2048)
     req = createCertRequest(pkey, CN=cname)
     cert = createCertificate(req, (cacert, cakey), 1, (0, 60*60*24*365*5)) # five years
-    print('Creating Certificate %s private key in \"simple/%s.pkey\"' % (fname, fname))
+    print('Creating Certificate %s private key in "simple/%s.pkey"' % (fname, fname))
     open('simple/%s.pkey' % (fname,), 'w').write(crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
-    print('Creating Certificate %s certificate in \"simple/%s.cert\"' % (fname, fname))
+    print('Creating Certificate %s certificate in "simple/%s.cert"' % (fname, fname))
     open('simple/%s.cert' % (fname,), 'w').write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
