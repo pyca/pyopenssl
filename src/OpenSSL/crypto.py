881dc8df9b05acef132ff182ab3c7e75a307a09a
Cory Benfield <lukasaoz@gmail.com>
2015-12-09 08:25:14 +0000

    Fix get_subject docstring.

--- src/OpenSSL/crypto.py
+++ src/OpenSSL/crypto.py
@@ -852,11 +852,13 @@ class X509Req(object):
         """
         Return the subject of this certificate signing request.
 
-        This creates a new :py:class:`X509Name`: modifying it does not affect
-        this request.
+        This creates a new :class:`X509Name` that wraps the underlying subject
+        name field on the certificate signing request. Modifying it will modify
+        the underlying signing request, and will have the effect of modifying
+        any other :class:`X509Name` that refers to this subject.
 
         :return: The subject of this certificate signing request.
-        :rtype: :py:class:`X509Name`
+        :rtype: :class:`X509Name`
         """
         name = X509Name.__new__(X509Name)
         name._name = _lib.X509_REQ_get_subject_name(self._req)
