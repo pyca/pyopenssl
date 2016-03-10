b11bffcc86a60e894761d5186760c1bae1530987
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-03-10 18:30:29 -0400

    bytes

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -3186,7 +3186,7 @@ class CRLTests(TestCase):
         """
         crl = CRL()
         revoked = Revoked()
-        revoked.set_serial("01")
+        revoked.set_serial(b"01")
         revoked.set_rev_date(b"20160310020145Z")
         crl.add_revoked(revoked=revoked)
         self.assertTrue(isinstance(crl.get_revoked()[0], Revoked))
