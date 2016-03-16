997630bbfa18cbcb564a9d647c555e97c11916f4
Cory Benfield <lukasaoz@gmail.com>
2016-03-16 09:52:09 +0000

    We don't need the whole history.

--- .travis/install_urllib3.sh
+++ .travis/install_urllib3.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-git clone https://github.com/shazow/urllib3.git
+git clone --depth 1 https://github.com/shazow/urllib3.git
 pip install -r ./urllib3/dev-requirements.txt
 pip install ./urllib3[socks]
