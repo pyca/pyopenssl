3439308fea0d3e2ed897eafe16316e09c1b8aa02
Cory Benfield <lukasaoz@gmail.com>
2016-11-29 09:30:07 +0000

    Allow clients to request OCSP

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -2122,6 +2122,18 @@ class Connection(object):
 
         return _ffi.buffer(data[0], data_len[0])[:]
 
+    def request_ocsp(self):
+        """
+        Called to request that the server sends stapled OCSP data, if
+        available. If this is not called on the client side then the server
+        will not send OCSP data. Should be used in conjunction with
+        :meth:`Context.set_ocsp_client_callback`.
+        """
+        rc = _lib.SSL_set_tlsext_status_type(
+            self._ssl, _lib.TLSEXT_STATUSTYPE_ocsp
+        )
+        _openssl_assert(rc == 1)
+
 
 ConnectionType = Connection
 
