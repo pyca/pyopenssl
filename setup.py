b93f1c3f8cd258dedc37b1137cc6a749555c550b
Moriyoshi Koizumi <mozo@mozo.jp>
2017-06-21 13:59:33 +0000

    Require cryptography >= 1.9

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         install_requires=[
             # Fix cryptographyMinimum in tox.ini when changing this!
-            "cryptography>=1.7",
+            "cryptography>=1.9",
             "six>=1.5.2"
         ],
     )
