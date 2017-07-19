29113b285773b06dd39c0a662ef531131aa50438
Paul Kehrer <paul.l.kehrer@gmail.com>
2017-07-19 13:57:47 +0200

    fix #664
    bytes and strings are different things.

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -731,8 +731,12 @@ class Context(object):
             _lib.X509_get_default_cert_file_env()
         ).decode("ascii")
         if not self._check_env_vars_set(dir_env_var, file_env_var):
-            default_dir = _ffi.string(_lib.X509_get_default_cert_dir())
-            default_file = _ffi.string(_lib.X509_get_default_cert_file())
+            default_dir = _ffi.string(
+                _lib.X509_get_default_cert_dir()
+            ).decode("ascii")
+            default_file = _ffi.string(
+                _lib.X509_get_default_cert_file()
+            ).decode("ascii")
             # Now we check to see if the default_dir and default_file are set
             # to the exact values we use in our manylinux1 builds. If they are
             # then we know to load the fallbacks
