99ea3b82676a360f6d30bda9ecfead7eb917b916
Kelby Ludwig <kelbyludwig@gmail.com>
2017-09-08 22:32:54 -0500

    updated tests to use bytestrings to avoid breaking python3 tests

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -3355,15 +3355,15 @@ class TestMemoryBIO(object):
         assert client_conn.client_random() != client_conn.server_random()
 
         # Export key material for other uses.
-        cekm = client_conn.export_keying_material("LABEL", 32)
-        sekm = server_conn.export_keying_material("LABEL", 32)
+        cekm = client_conn.export_keying_material(b'LABEL', 32)
+        sekm = server_conn.export_keying_material(b'LABEL', 32)
         assert cekm is not None
         assert sekm is not None
         assert cekm == sekm
         assert len(sekm) == 32
 
-        cekmc = client_conn.export_keying_material("LABEL", 32, "CONTEXT")
-        sekmc = server_conn.export_keying_material("LABEL", 32, "CONTEXT")
+        cekmc = client_conn.export_keying_material(b'LABEL', 32, b'CONTEXT')
+        sekmc = server_conn.export_keying_material(b'LABEL', 32, b'CONTEXT')
         assert cekmc is not None
         assert sekmc is not None
         assert cekmc == sekmc
