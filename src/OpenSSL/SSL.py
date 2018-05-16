33f0cb5023f52d1043613986f78a5e24ae367228
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-05-16 12:03:07 -0400

    ignore any args/kwargs passed

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -2019,7 +2019,7 @@ class Connection(object):
             result.append(pyname)
         return result
 
-    def makefile(self, mode=None, bufsize=None):
+    def makefile(self, *args, **kwargs):
         """
         The makefile() method is not implemented, since there is no dup
         semantics for SSL connections
