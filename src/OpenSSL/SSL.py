930434efda53a8ab80d06d9ade9d8235d3f25fee
Daniel Holth <dholth@fastmail.fm>
2019-07-08 18:05:21 -0400

    context-managed from_buffer + black

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -15,6 +15,7 @@ from OpenSSL._util import (
     UNSPECIFIED as _UNSPECIFIED,
     exception_from_error_queue as _exception_from_error_queue,
     ffi as _ffi,
+    from_buffer as _from_buffer,
     lib as _lib,
     make_assert as _make_assert,
     native as _native,
@@ -1725,14 +1726,18 @@ class Connection(object):
         # Backward compatibility
         buf = _text_to_bytes_and_warn("buf", buf)
 
-        buf = _ffi.from_buffer(buf)
+        with _from_buffer(buf) as buf:
 
-        if len(buf) > 2147483647:
-            raise ValueError("Cannot send more than 2**31-1 bytes at once.")
+            # THE TEST MOCK HAS ZERO BYTES HERE
 
-        result = _lib.SSL_write(self._ssl, buf, len(buf))
-        self._raise_ssl_error(self._ssl, result)
+            if len(buf) > 2147483647:
+                raise ValueError("Cannot send more than 2**31-1 bytes at once.")
+
+            result = _lib.SSL_write(self._ssl, buf, len(buf))
+            self._raise_ssl_error(self._ssl, result)
+            
         return result
+
     write = send
 
     def sendall(self, buf, flags=0):
@@ -1748,28 +1753,24 @@ class Connection(object):
         """
         buf = _text_to_bytes_and_warn("buf", buf)
 
-        if isinstance(buf, memoryview):
-            buf = buf.tobytes()
-        if isinstance(buf, _buffer):
-            buf = str(buf)
-        if not isinstance(buf, bytes):
-            raise TypeError("buf must be a memoryview, buffer or byte string")
+        with _from_buffer(buf) as data:
+            
+            left_to_send = len(buf)
+            total_sent = 0
 
-        left_to_send = len(buf)
-        total_sent = 0
-        data = _ffi.new("char[]", buf)
+            while left_to_send:
+                # SSL_write's num arg is an int,
+                # so we cannot send more than 2**31-1 bytes at once.
+                result = _lib.SSL_write(
+                    self._ssl,
+                    data + total_sent,
+                    min(left_to_send, 2147483647)
+                )
+                self._raise_ssl_error(self._ssl, result)
+                total_sent += result
+                left_to_send -= result
 
-        while left_to_send:
-            # SSL_write's num arg is an int,
-            # so we cannot send more than 2**31-1 bytes at once.
-            result = _lib.SSL_write(
-                self._ssl,
-                data + total_sent,
-                min(left_to_send, 2147483647)
-            )
-            self._raise_ssl_error(self._ssl, result)
-            total_sent += result
-            left_to_send -= result
+        return total_sent
 
     def recv(self, bufsiz, flags=None):
         """
