a4ac4e4493625cdf7047d0440bbe383e0ce1ee42
Mark Williams <mrw@enotuniq.org>
2019-11-17 18:23:43 -0800

    Update src/OpenSSL/SSL.py
    Co-Authored-By: Alex Gaynor <alex.gaynor@gmail.com>

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -1507,7 +1507,7 @@ class Context(object):
             one of those bytestrings to indicate the chosen protocol, the
             empty bytestring to terminate the TLS connection, or the
             :py:obj:`NO_OVERLAPPING_PROTOCOLS` to indicate that no offered
-            protocol was selected.
+            protocol was selected, but that the connection should not be aborted.
         """
         self._alpn_select_helper = _ALPNSelectHelper(callback)
         self._alpn_select_callback = self._alpn_select_helper.callback
