8a1bc02849fd562e2d530295638784e63a0d10c4
Alex Gaynor <alex.gaynor@gmail.com>
2019-12-14 11:07:32 -0500

    Drop support for Python 3.4, since cryptography dropped it

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
@@ -89,6 +88,7 @@ if __name__ == "__main__":
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: System :: Networking',
         ],
+        python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 
         packages=find_packages(where="src"),
         package_dir={"": "src"},
