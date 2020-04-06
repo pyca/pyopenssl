9ac2ba15188c438f69003e5c9b0e2be6fe60f822
Joseba Alberdi <j.alberdi@simuneatomistics.com>
2020-04-06 16:12:20 +0200

    Fixing issue #798, thanks to @reaperhulk; removing undocumented '%s' option and getting the date in a more robust way

--- src/OpenSSL/crypto.py
+++ src/OpenSSL/crypto.py
@@ -1641,10 +1641,11 @@ class X509Store(object):
         :param datetime vfy_time: The verification time to set on this store.
         :return: ``None`` if the verification time was successfully set.
         """
+        import calendar
         param = _lib.X509_VERIFY_PARAM_new()
         param = _ffi.gc(param, _lib.X509_VERIFY_PARAM_free)
 
-        _lib.X509_VERIFY_PARAM_set_time(param, int(vfy_time.strftime('%s')))
+        _lib.X509_VERIFY_PARAM_set_time(param, calendar.timegm(vfy_time.timetuple()))
         _openssl_assert(_lib.X509_STORE_set1_param(self._store, param) != 0)
 
 
