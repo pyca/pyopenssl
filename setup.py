de2dbf7c56b18ddc69d797b64859d57949dbf310
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-11-27 15:47:04 -0600

    20.0.0 before 2020 is even over (#968)

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Security :: Cryptography",
