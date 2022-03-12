40e865b1992ba7b6e890f96539b6bdf1051e4def
Alex Gaynor <alex.gaynor@gmail.com>
2022-03-12 16:23:00 -0500

    Remove call that hasn't been required since OpenSSL 1.1.0 (#1107)

--- src/OpenSSL/crypto.py
+++ src/OpenSSL/crypto.py
@@ -3252,11 +3252,6 @@ load_pkcs12 = utils.deprecated(
 # using)).
 _lib.OpenSSL_add_all_algorithms()
 
-# This is similar but exercised mainly by exception_from_error_queue.  It calls
-# both ERR_load_crypto_strings() and ERR_load_SSL_strings().
-_lib.SSL_load_error_strings()
-
-
 # Set the default string mask to match OpenSSL upstream (since 2005) and
 # RFC5280 recommendations.
 _lib.ASN1_STRING_set_default_mask_asc(b"utf8only")
