b08864a14a278f5bacb3d9dfd0437f21212560c7
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-05-12 06:01:41 +0800

    use constant

--- src/OpenSSL/SSL.py
+++ src/OpenSSL/SSL.py
@@ -1691,12 +1691,13 @@ class Connection:
             # raise syscall if it is an EOF.
             peeked_error = _lib.ERR_peek_error()
             reason = _lib.ERR_GET_REASON(peeked_error)
-            # 294 is SSL_R_UNEXPECTED_EOF_WHILE_READING in OpenSSL 3.0.x
-            if reason == 294:
+            try:
+                _openssl_assert(
+                    reason == _lib.SSL_R_UNEXPECTED_EOF_WHILE_READING
+                )
                 _lib.ERR_clear_error()
                 raise SysCallError(-1, "Unexpected EOF")
-            else:
-                # TODO: This is untested.
+            except AttributeError:
                 _raise_current_error()
         elif error == _lib.SSL_ERROR_NONE:
             pass
