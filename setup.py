288010cdf9705483e5db3e1174782800180ec6b1
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-30 19:54:18 +0300

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Security :: Cryptography",
