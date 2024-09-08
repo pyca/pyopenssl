6502a0edea6930d3756d1b27f5c3a9c729b5aa86
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-09-08 20:36:37 +0300

    Add support for Python 3.13 (#1363)

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Security :: Cryptography",
