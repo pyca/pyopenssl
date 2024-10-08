6aed5048254963c631d6af70b7f180af66eafdec
Alex Gaynor <alex.gaynor@gmail.com>
2024-10-08 06:57:17 -0400

    update for latest sphinx rtd theme release

--- doc/conf.py
+++ doc/conf.py
@@ -132,7 +132,6 @@ if os.environ.get("READTHEDOCS", None) == "True":
     html_context["READTHEDOCS"] = True
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
