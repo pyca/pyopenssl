54482bfe22ebd0c7aae16b08218cef408d3bd97f
Alex Gaynor <alex.gaynor@gmail.com>
2025-05-17 09:19:19 -0700

    Bump for 25.1.0 release

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
         packages=find_packages(where="src"),
         package_dir={"": "src"},
         install_requires=[
-            "cryptography>=41.0.5,<45",
+            "cryptography>=41.0.5,<46",
             (
                 "typing-extensions>=4.9; "
                 "python_version < '3.13' and python_version >= '3.8'"
